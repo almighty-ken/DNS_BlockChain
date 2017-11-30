2b210394ef0ae4b86153a6803d4680d763bfd4bb
kencheng <kenny851017@gmail.com>
2017-11-29 22:51:09 -0500

    Basic implementation of blockchain complete

--- blockchain.py
+++ blockchain.py
@@ -99,17 +99,104 @@ class Blockchain(object):
 		salt = next(salt_gen)
 		while not self.valid_proof(last_proof,salt):
 			salt = next(salt_gen)
-
 		return salt
 
+	def new_transaction(self,transaction):
+		"""
+		Creates a new transaction to go into the next mined Block
+		For flexibility, we do not define the format of transactions here
 
+		:param transaction: the new transaction we are appending
+		:return: The index of the Block that will hold this transaction
+		"""
+		self.current_transactions.append(transaction)
+		return self.last_block['index']+1
 
+	def new_block(self,proof,previous_hash):
+        """
+        Create a new Block in the Blockchain
 
+        :param proof: The proof given by the Proof of Work algorithm
+        :param previous_hash: Hash of previous Block
+        :return: New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
 
+        # Reset the current list of transactions
+        self.current_transactions = []
 
+        self.chain.append(block)
+        return block        
 
+    def resolve_conflicts(self):
+    	"""
+    	This is our consensus algorithm, it resolves conflicts
+    	by replacing our chain with the longest one in the network.
 
+    	:return: True if our chain was replaced, False if not
+    	"""
 
+    	neighbours = self.nodes
+    	new_chain = None
+
+    	# We're only looking for chains longer than ours
+    	max_length = len(self.chain)
+
+    	# Grab and verify the chains from all the nodes in our network
+    	for node in neighbours:
+    	    response = requests.get(f'http://{node}/chain')
+
+    	    if response.status_code == 200:
+    	        length = response.json()['length']
+    	        chain = response.json()['chain']
+
+    	        # Check if the length is longer and the chain is valid
+    	        if length > max_length and self.valid_chain(chain):
+    	            max_length = length
+    	            new_chain = chain
+
+    	# Replace our chain if we discovered a new, valid chain longer than ours
+    	if new_chain:
+    	    self.chain = new_chain
+    	    return True
+
+    	return False
+
+    @classmethod
+    def valid_chain(cls,chain):
+    	"""
+    	Determine if a given blockchain is valid
+
+    	:param chain: A blockchain
+    	:return: True if valid, False if not
+    	"""
+
+    	last_block = chain[0]
+    	current_index = 1
+
+    	while current_index < len(chain):
+    	    block = chain[current_index]
+    	    print(f'{last_block}')
+    	    print(f'{block}')
+    	    print("\n-----------\n")
+    	    # Check that the hash of the block is correct
+    	    if block['previous_hash'] != cls.hash(last_block):
+    	        return False
+
+    	    # Check that the Proof of Work is correct
+    	    if not cls.valid_proof(last_block['proof'], block['proof']):
+    	        return False
+
+    	    last_block = block
+    	    current_index += 1
+
+    	return True
 
 
 
